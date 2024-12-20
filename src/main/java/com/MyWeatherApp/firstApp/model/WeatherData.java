687f7e7d1d3a4061143c23e61013afd8aa7ff71c
smnsohail <1smnsohail@gmail.com>
2024-12-20 17:07:51 +0530

    newly added humidity, windspeed, feelsLike

--- src/main/java/com/MyWeatherApp/firstApp/model/WeatherData.java
+++ src/main/java/com/MyWeatherApp/firstApp/model/WeatherData.java
@@ -1,31 +1,54 @@
-//MODEL CLASS FILE
-
 package com.MyWeatherApp.firstApp.model;
 
 public class WeatherData {
-    
     private String cityName;
     private double temperature;
+    private double feelsLike; // New field for feels like temperature
+    private double humidity;
     private String description;
+    private double windSpeed;
 
-    // generate getters and setters for all above instance fileds to get access
+    // Getters and setters for all fields..
+
     public String getCityName() {
         return cityName;
     }
     public void setCityName(String cityName) {
         this.cityName = cityName;
     }
+   
     public double getTemperature() {
         return temperature;
     }
     public void setTemperature(double temperature) {
         this.temperature = temperature;
     }
+    
+    public double getFeelsLike() {
+        return feelsLike;
+    }
+    public void setFeelsLike(double feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+   
+    public double getHumidity() {
+        return humidity;
+    }
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+   
     public String getDescription() {
         return description;
     }
     public void setDescription(String description) {
         this.description = description;
     }
-    
+   
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
 }
